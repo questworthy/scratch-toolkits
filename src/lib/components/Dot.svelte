cb00e03a4f7dd3d675819e79a1cf2f665061e23a
commitsovercoffee <commitsovercoffee@gmail.com>
2024-07-12 01:39:08 +0530

    adds cursor-follower

---
+++ src/lib/components/Dot.svelte
@@ -0,0 +1,27 @@
+<script>
+	import { spring } from 'svelte/motion';
+
+	let coords = spring(
+		{ x: 50, y: 50 },
+		{
+			stiffness: 0.1,
+			damping: 0.9
+		}
+	);
+
+	let size = spring(20);
+</script>
+
+<svelte:window
+	on:mousemove={(e) => {
+		coords.set({ x: e.clientX, y: e.clientY });
+	}}
+	on:mousedown={() => {
+		size.set(40);
+	}}
+	on:mouseup={() => size.set(20)}
+/>
+
+<svg class="absolute opacity-40 -z-50" role="presentation" height="100%" width="100%">
+	<circle class="fill-amber-400" cx={$coords.x} cy={$coords.y} r={$size} />
+</svg>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,22 @@
 <script>
 	import '../app.css';
-
 	import '@fontsource/comic-neue/300.css';
 	import '@fontsource/comic-neue/400.css';
 	import '@fontsource/comic-neue/700.css';
+
+	import Dot from '$lib/components/Dot.svelte';
+
+	import { onMount } from 'svelte';
+
+	let width = 0;
+	let height = 0;
+	onMount(() => {
+		width = screen.width;
+		height = screen.height;
+	});
 </script>
 
+{#if width >= 1024 && width > height}
+	<Dot />
+{/if}
 <slot />
