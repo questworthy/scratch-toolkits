4e90ae4dbea27563262eb362e767606480f67fdf
commitsovercoffee <commitsovercoffee@gmail.com>
2024-07-16 17:50:55 +0530

    adds confetti for final lesson

--- src/routes/toolkits/gender-equity/level-5/step-5/+page.svelte
+++ src/routes/toolkits/gender-equity/level-5/step-5/+page.svelte
@@ -1,8 +1,8 @@
 <script>
 	import { onMount } from 'svelte';
 	import { appState } from '$lib/stores';
+	import { Confetti } from 'svelte-confetti';
 	import Button from '$lib/components/Button.svelte';
-	import PostIt from '$lib/components/PostIt.svelte';
 
 	let online = false;
 	let hidden = false;
@@ -13,17 +13,13 @@
 			hidden = true;
 		}, 1000);
 
-		$appState.dialogue = 'We believe that you can bring about a revolution. 👮';
+		$appState.expression = 'smile';
+		$appState.dialogue = '🎉 Congrats ! You have completed the course !';
 		$appState.title = '🎨 Step 5 : Build your own Scratch project!';
 		$appState.leftButtonLink = '/toolkits/gender-equity/level-5/step-4';
 		$appState.rightButtonLink = '';
+		$appState.rightButtonLink = '';
 	});
-
-	const posts = [
-		'It’s Normal to Question: It’s perfectly normal to have questions about your sexual identity. It’s a part of growing up and figuring out who you are.',
-		'Be Respectful: Just like we respect people’s choices in other aspects of life, we should also respect their sexual identity. Name-calling or making fun of someone is never okay.',
-		'Talk to Someone: If you have questions or feel confused, talk to someone you trust, like a parent, teacher, or school counselor. They can offer support and guidance.'
-	];
 </script>
 
 <div class="mx-auto max-w-screen-md prose m-4 p-4 prose-xl">
@@ -37,4 +33,27 @@
 
 	<Button label="Create your own scratch project" />
 	<Button base={false} label="Remix the project earlier used in this level" />
+</div>
+
+<div
+	style="
+ position: fixed;
+ top: -50px;
+ left: 0;
+ height: 100vh;
+ width: 100vw;
+ display: flex;
+ justify-content: center;
+ overflow: hidden;
+ pointer-events: none;"
+>
+	<Confetti
+		x={[-5, 5]}
+		y={[0, 0.1]}
+		delay={[500, 2000]}
+		infinite
+		duration={5000}
+		amount={200}
+		fallDistance="100vh"
+	/>
 </div>
